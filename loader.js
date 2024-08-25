08be18179963a531751184c10af866c517cf26d4
Karzoull <hdckarim@gmail.com>
2024-08-25 16:38:27 +0200

    dsd

--- loader.js
+++ loader.js
@@ -1,10 +1,8 @@
 
 window.addEventListener('load', function() {
     const preloader = document.getElementById('preloader');
-    const mainContent = document.getElementById('main-content');
 
     setTimeout(function() {
         preloader.style.display = 'none';
-        mainContent.style.display = 'block';
-    }, 3000);
+    }, 500);
 });
